ad1eff3ddc802ffaf7b15d971c4410f88319751e
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-08-18 08:59:56 +0800

    Adder Rotation to Inspector

--- engine/Editor/SceneEditor.cpp
+++ engine/Editor/SceneEditor.cpp
@@ -79,16 +79,31 @@ void SceneEditor::DrawInspector()
 
 		//TRANSFORM SETTINGS
 		ImGui::SeparatorText("Transform");
-		float tmpX = inspectedObject->position.x;
-		float tmpY = inspectedObject->position.y;
-		float tmpZ = inspectedObject->position.z;
-		ImGui::DragFloat("x position", &tmpX, 0.1f);
-		ImGui::DragFloat("y position", &tmpY, 0.1f);
-		ImGui::DragFloat("z position", &tmpZ, 0.1f);
+		float tmpPosX = inspectedObject->position.x;
+		float tmpPosY = inspectedObject->position.y;
+		float tmpPosZ = inspectedObject->position.z;
+		ImGui::Text("Position:");
+		ImGui::DragFloat("x position", &tmpPosX, 0.1f);
+		ImGui::DragFloat("y position", &tmpPosY, 0.1f);
+		ImGui::DragFloat("z position", &tmpPosZ, 0.1f);
 
-		if (tmpX != inspectedObject->position.x || tmpY != inspectedObject->position.y || tmpZ != inspectedObject->position.z) {
-			inspectedObject->SetPosition({ tmpX, tmpY, tmpZ });
+		if (tmpPosX != inspectedObject->position.x || tmpPosY != inspectedObject->position.y || tmpPosZ != inspectedObject->position.z) {
+			inspectedObject->SetPosition({ tmpPosX, tmpPosY, tmpPosZ });
 		}
+
+		
+		float tmpRotX = inspectedObject->rotation.x;
+		float tmpRotY = inspectedObject->rotation.y;
+		float tmpRotZ = inspectedObject->rotation.z;
+		ImGui::Text("Rotation:");
+		ImGui::DragFloat("x rotation", &tmpRotX, 0.1f);
+		ImGui::DragFloat("y rotation", &tmpRotY, 0.1f);
+		ImGui::DragFloat("z rotation", &tmpRotZ, 0.1f);
+
+		if (tmpRotX != inspectedObject->position.x || tmpRotY != inspectedObject->position.y || tmpRotZ != inspectedObject->position.z) {
+			inspectedObject->SetRotation({ tmpRotX, tmpRotY, tmpRotZ });
+		}
+
 
 		//PHYSICS SETTINGS
 		ImGui::SeparatorText("Physics");
