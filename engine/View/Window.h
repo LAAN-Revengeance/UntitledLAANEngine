0d10a5276e8eb63390906f187fc4e420ec3a9c24
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-09-18 17:53:17 +0800

    Started GLFW window facade

---
+++ engine/View/Window.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <string>
+#include "Graphics/Graphics.h"
+
+typedef void(*WindowResizeFunc)(int width, int height);
+
+enum WindowType {
+	WINDOW_TYPE_WINDOWED,
+	WINDOW_TYPE_BORDERLESS,
+	WINDOW_TYPE_FULLSCREEN
+};
+
+/**
+*	@Class Window
+*	@brief Used to manage a window and its rendering context.
+*
+*	@author Andres Comeros-Ochtman
+*	@version 1.0
+*	@date 18/09/2023
+*/
+class Window
+{
+public:
+	Window(int w, int h,const std::string& wName);
+	~Window();
+
+	bool IsWindowClosed();
+	void CloseWindow();
+
+	void SwapBuffers();
+	void PollEvents();
+
+	void SetResizeCallback(WindowResizeFunc callback);
+
+	void MakeCurrentContext();
+
+	int GetWidth();
+	int GetHeight();
+
+	void SetName(std::string nName);
+	void SetIcon(std::string path);
+	void SetWindowType(WindowType type);
+
+private:
+	static void _mGlFWCallback(GLFWwindow* window, int width, int height);
+	WindowResizeFunc* resizeCallback = nullptr;
+
+	static bool _glfwInit;
+
+	GLFWwindow* window;
+	std::string name;
+
+	static std::vector<GLFWwindow*> activeWindows;
+};
+
