b31abb0a9c3ee932207522703f4052429b2085f3
Aidan O'Connor <aidantjoc@gmail.com>
2023-10-29 12:10:09 +0800

    Tested multiple FCM relationships

--- engine/Controller/EmotionManager.cpp
+++ engine/Controller/EmotionManager.cpp
@@ -3,9 +3,11 @@
 void EmotionManager::InitFCM()
 {
 	FCM fcm;
-	fcm.AddConcept("Attack", 1, 1);
+	fcm.AddConcept("Attack", 0.5, 1);
+	fcm.AddConcept("Likelihood", 0.8, 1);
 	fcm.AddConcept("Anger", 0, 1);
-	fcm.AddRelationship("Attack", "Anger", 1);
+	fcm.AddRelationship("Attack", "Likelihood", 1);
+	fcm.AddRelationship("Likelihood", "Anger", 1);
 
 	fcms.push_back(fcm);
 
