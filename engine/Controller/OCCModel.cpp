f0bc9621332ea3889667786c19007e0813c1b0cd
Aidan O'Connor <aidantjoc@gmail.com>
2023-10-31 11:21:09 +0800

    Added personality factors to emotion FCM

--- engine/Controller/OCCModel.cpp
+++ engine/Controller/OCCModel.cpp
@@ -22,15 +22,19 @@ void OCCModel::InitAngerFCM(std::string eventName, float eventStrength, std::str
 	FCM attackFCM;
 	attackFCM.AddConcept(eventName, eventStrength, 1);
 	attackFCM.AddConcept("Desireability", 0, 1);
+	attackFCM.AddConcept("Trust", 0, 1);
 	attackFCM.AddConcept("Blameworthiness", 0, 1);
-	attackFCM.AddConcept("Cope", 0, 1);
+	attackFCM.AddConcept("Irritability", 0, 1);
 	attackFCM.AddConcept(emotion, 0, 1);
+	attackFCM.AddConcept("Impulsivity", 0, 1);
 	attackFCM.AddConcept("Action", 0, 1);
-	attackFCM.AddRelationship(eventName, "Blameworthiness", 1);
+	attackFCM.AddRelationship(eventName, "Trust", npcPersonality.GetAgreeableness());
+	attackFCM.AddRelationship("Trust", "Blameworthiness", 1);
 	attackFCM.AddRelationship(eventName, "Desireability", -1);
-	attackFCM.AddRelationship("Blameworthiness", "Cope", 2);
-	attackFCM.AddRelationship("Desireability", "Cope", -1);
-	attackFCM.AddRelationship("Cope", emotion, npcPersonality.GetNeuroticismPercent());
+	attackFCM.AddRelationship("Blameworthiness", "Irritability", 2);
+	attackFCM.AddRelationship("Desireability", "Irritability", -1);
+	attackFCM.AddRelationship("Irritability", emotion, npcPersonality.GetNeuroticismPercent());
+	attackFCM.AddRelationship(emotion, "Impulsivity", npcPersonality.GetConscientiousnessPercent());
 	attackFCM.AddRelationship(emotion, "Action", 1);
 
 	this->fcm = attackFCM;
@@ -46,10 +50,13 @@ void OCCModel::InitFearFCM(std::string eventName, float eventStrength, std::stri
 	fearFCM.AddConcept("Likelihood", 0, 1);
 	fearFCM.AddConcept("Fear", 0, 1);
 	fearFCM.AddConcept("Action", 0, 1);
-	fearFCM.AddRelationship(eventName, "Likelihood", -1);
-	fearFCM.AddRelationship(eventName, "Desireability", 1);
-	fearFCM.AddRelationship("Likelihood", emotion, 2);
-	fearFCM.AddRelationship("Desireability", emotion, -1);
+	fearFCM.AddRelationship(eventName, "Trust", npcPersonality.GetAgreeableness());
+	fearFCM.AddRelationship("Trust", "Likelihood", 1);
+	fearFCM.AddRelationship(eventName, "Desireability", -1);
+	fearFCM.AddRelationship("Likelihood", "Irritability", 2);
+	fearFCM.AddRelationship("Desireability", "Irritability", -1);
+	fearFCM.AddRelationship("Irritability", emotion, npcPersonality.GetNeuroticismPercent());
+	fearFCM.AddRelationship(emotion, "Impulsivity", npcPersonality.GetConscientiousnessPercent());
 	fearFCM.AddRelationship(emotion, "Action", 1);
 
 	this->fcm = fearFCM;
