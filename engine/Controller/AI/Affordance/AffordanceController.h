108b015f39f12023991b7a7154c8072b406d2594
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-11-02 18:12:00 +0800

    Affordance controller comments

--- engine/Controller/AI/Affordance/AffordanceController.h
+++ engine/Controller/AI/Affordance/AffordanceController.h
@@ -3,33 +3,83 @@
 #include "Global_Affordances.h"
 #include <vector>
 
+/**
+*	@Class AffordanceController
+*	@brief Used to control an objects available affordance
+*
+*	@author Andres Comeros-Ochtman, Lochy Edward
+*	@version 1.0
+*	@date 15/10/2023
+*/
 class AffordanceController
 {
 public:
+	/**
+	 * @brief Constructor. Requires an owner to function properly
+	 * @param owner object that owns this affordance controller
+	*/
 	AffordanceController(GameObject* owner);
-
+	
+	/**
+	 * @brief destuctor
+	*/
 	~AffordanceController();
 
+	/**
+	 * @brief Updates all the affordances added to this object
+	 * @param deltaTime time since last frame
+	*/
 	void Update(double deltaTime);
-
+	
+	/**
+	 * @brief Add an affordance type to this object
+	 * @tparam T affordance type
+	 * @return the affordacnce added to this object
+	*/
 	template<class T>
 	T* AddAffordance();
 
+	/**
+	 * @brief Get an afforance from this object
+	 * @tparam T the affordance type retrieving
+	 * @return affordance of type T retrieved
+	*/
 	template<class T>
 	T* GetAffordance();
 
+	/**
+	 * @brief Remove an affordance from this object
+	 * @tparam T the affordance type to remove
+	*/
 	template<class T>
 	void RemoveAffordance();
 
-	//Lua implementations
+	/**
+	 * @brief Returns an affordance based on its name
+	 * @param type the name of the affordace being retrieved
+	 * @return affordance with name type, will return a
+	 * blank affordance if affordance does not exist
+	*/
 	Affordance* GetAffordanceString(const std::string type);
+
+	/**
+	 * @brief Remove an affordance from this controller based on its name
+	 * @param type name of affordance to remove
+	*/
 	void RemoveAffordanceString(const std::string type);
 
 private:
 
+	///The ower of this affordance controller
 	GameObject* _owner;
+
+	///List of all affordances the owner has
 	std::vector<Affordance*> _affordances;
+
+	///Empty affordance to return of no affordance is available
 	static AffordanceBlank _blankAffordance;
+
+
 	friend class SceneLoader;
 };
 
